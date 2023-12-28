76b7360cd41fbc9d904233c5fab1de5db9d120cf
Sathishkumar <sathish.visar@gmail.com>
2023-12-28 14:58:16 +0530

    code optimize & products UI fix, update all svg icons to IconFunction

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,5 +1,7 @@
 import React from "react";
 import classnames from "classnames";
+import Rating from '@mui/material/Rating';
+import IconAddToCart from '@components/icons/IconAddToCart';
 import BgImage from '@assets/images/category.png';
 import "./Product.scss"
 
@@ -12,6 +14,16 @@ export default function Product (props) {
         </div>
         <div className="info">
             <h4>{name || 'Name'}</h4>
+            <div className={classnames('d-flex flex-align-center flex-justify-between')}>
+                <Rating
+                    className="productRating"
+                    name="simple-controlled"
+                    value={rating || 0}
+                    size="small"
+                />
+                <span className="addTocart"><IconAddToCart /></span>
+            </div>
+            
             <h6>$ {price || 0}</h6>
         </div>
     </div>
